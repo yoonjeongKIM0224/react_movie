8ec17edf23f4edebe7c76544c805443dadedff5d
kimyoonjeong <gnoejnooy@gmail.com>
2024-07-19 00:47:49 +0900

    반응형 작업

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div class="wrap">
+  <div className="wrap">
     <App />
   </div>
 );
